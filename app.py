28ce7bfaf408cd750244381c2185fac85f5c086b
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-01-10 08:29:41 -0500

    Create Stores in REST API

--- app.py
+++ app.py
@@ -18,3 +18,10 @@ stores = [
 def get_stores():
     return {"stores": stores}
 
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data['name'], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
