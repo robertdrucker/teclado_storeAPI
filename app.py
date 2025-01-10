c58f894a920d37728b8fd86127505d46fb5bbcc7
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-01-10 18:29:57 -0500

    General improvements to REST API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -11,6 +12,16 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request.  Ensure 'name' is included in the JSON payload"
+        )
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -19,8 +30,26 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request.  Ensure 'price', 'store_id', and 'name' are included in the JSON payload.", 
+        )
+   
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, msessage="Item already exists.")
+
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -28,16 +57,12 @@ def create_item():
 
     return item, 201
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+          abort(404, message="Store not found.")
 
 
 @app.get("/item/<string:item_id>")
@@ -45,4 +70,8 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"Message": "Item not found"}, 404
+          abort(404, message="Item not found.")
+    
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
