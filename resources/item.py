333a1cb0d159dfcd86495f13a0f9dceee16f27dd
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-01-11 15:46:31 -0500

    Decorate responses with Flask-Smorest

--- resources/item.py
+++ resources/item.py
@@ -1,16 +1,17 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
+from schemas import ItemSchema, ItemUpdateSchema
 from db import items
 
-blp = Blueprint("Items", __name__, description="Operations on items")
+blp = Blueprint("Items", "items", description="Operations on items")
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
     def get(self, item_id):
+        # print(f'items via get /item/id: {items}')
         try:
             return items[item_id]
         except KeyError:
@@ -23,49 +24,41 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request.  Ensure 'price' and 'name' are included in the JSON payload.")
-
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
+
+            # https://blog.teclado.com/python-dictionary-merge-update-operators/
             item |= item_data
             return item
         except KeyError:
-            abort(404, message="Item not found")
+            abort(404, message="Item not found.")
 
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values() 
-    
-    def post(self, item_data):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request.  Ensure 'price', 'store_id', and 'name' are included in the JSON payload.", 
-            )
-    
+        # print('items.values: ', items.values)
+        # items.values:  <built-in method values of dict object at 0x106118400>
+        # return {"items": list(items.values())}
+
+        # ItemSchema(many=True) above turns the response into a list of items instead of an object of items
+        return items.values()
+
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(cls, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
                 and item_data["store_id"] == item["store_id"]
             ):
-                abort(400, msessage="Item already exists.")
-
-
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found.")
+                abort(400, message=f"Item already exists.")
 
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
         items[item_id] = item
-
         return item
--- resources/store.py
+++ resources/store.py
@@ -9,12 +9,14 @@ from flask.views import MethodView
 
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 blp = Blueprint("Stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
-    def get(self, store_id):
+    @blp.response(200, StoreSchema)
+    def get(cls, store_id):
         try:
             # You presumably would want to include the store's items here too
             # More on that when we look at databases
@@ -22,31 +24,29 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Store not found.")
 
-    def delete(self, store_id):
+    def delete(cls, store_id):
         try:
             del stores[store_id]
             return {"message": "Store deleted."}
         except KeyError:
             abort(404, message="Store not found.")
 
-    @blp.route("/store")
-    class StoreList(MethodView):
-        def get(self):
-            return {"stores": list(stores.values())}
 
-        def post(self):
-            store_data = request.get_json()
-            if "name" not in store_data:
-                abort(
-                    400,
-                    message="Bad request.  Ensure 'name' is included in the JSON payload"
-                )
+@blp.route("/store")
+class StoreList(MethodView):
+    def get(self):
+        return stores.values()
 
-            for store in stores.values():
-                if store_data["name"] == store["name"]:
-                    abort(400, message="Store already exists.")
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(cls, store_data):
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message=f"Store already exists.")
 
-            store_id = uuid.uuid4().hex
-            store = {**store_data, "id": store_id}
-            stores[store_id] = store
-            return store
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+
+        return store
+
