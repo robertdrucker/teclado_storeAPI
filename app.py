d08dffd24deceb7230f69166cdbbabf15ae89cfe
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-01-11 10:49:53 -0500

    Switch to Blueprints and MethodViews

--- app.py
+++ app.py
@@ -1,107 +1,40 @@
-import uuid
-from flask import Flask, request
-from flask_smorest import abort
-from db import items, stores
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-          abort(404, message="Store not found.")
+# This is a Flask configuration that handles propogation of 
+# exceptions hidden within Flask extension(s) so that they are 
+# or are not propogated into the main app so we can see it.
+app.config["PROPAGATE_EXCEPTIONS"] = True
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request.  Ensure 'name' is included in the JSON payload"
-        )
+# Flask smorest configuation
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
 
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message="Store already exists.")
+# OPENAPI is a standard for API documentation.
+# This tells Flask smorest to use 3.0.3.
+app.config["OPENAPI_VERSION"] = "3.0.3"
 
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
+# Tells Flask smorest where the root of the API is.
+app.config["OPENAPI_URL_PREFIX"] = "/"
 
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "Store deleted."}
-    except KeyError:
-        abort(404, message="Store not found.")
+# Documentation configurations for swagger-ui path
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+# This tells Flask smorest to use swagger-ur for the 
+# documentation that is in /swagger-ui.
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-          abort(404, message="Item not found.")
+# cdn for swagger documentation code
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad request.  Ensure 'price', 'store_id', and 'name' are included in the JSON payload.", 
-        )
-   
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, msessage="Item already exists.")
+# Connects the flask smorest extension to the Flask app.
+api = Api(app)
 
-
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found.")
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item, 201
-
-@app.delete("/item/<string:item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "Item deleted"}
-    except KeyError:
-        abort(404, message="Item not found.")
-
-@app.put("/item/<string:item_id>")
-def update_item(item_id):
-    item_data = request.get_json()
-    if "price" not in item_data or "name" not in item_data:
-        abort(400, message="Bad request.  Ensure 'price' and 'name' are included in the JSON payload.")
-
-    try:
-        item = items[item_id]
-        item |= item_data
-        return item
-    except KeyError:
-        abort(404, message="Item not found")
-
-
+# register blueprints
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
