ae130b42c011c02244c78bad8dd425bf693b8789
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-01-12 15:41:52 -0500

    Add many-to-many relationships between stores and tags

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from models.item import ItemModel
 from models.store import StoreModel
 from models.tag import TagModel
+from models.item_tags import ItemsTags
 
--- schemas.py
+++ schemas.py
@@ -41,3 +41,8 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
